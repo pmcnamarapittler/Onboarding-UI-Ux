9a97a4e2d1de92f9b3be8e01d7b3959b042df695
Paige McNamara-Pittler <pemcnama@usc.edu>
2025-09-16 15:14:02 -0700

    Add files via upload

---
+++ JotItDownApp/JotItDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  JotItDownApp.swift
+//  JotItDownApp
+//
+//  Created by Paige McNamara-Pittler on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct JotItDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
