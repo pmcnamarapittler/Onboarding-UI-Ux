0acc050803a76a4fae0aac2fbd7b490adf97ab54
Paige McNamara-Pittler <pemcnama@usc.edu>
2025-09-16 07:47:38 -0700

    Initial Commit

---
+++ JotItDownApp/JotItDownAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JotItDownAppApp.swift
+//  JotItDownApp
+//
+//  Created by Paige McNamara-Pittler on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct JotItDownAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
