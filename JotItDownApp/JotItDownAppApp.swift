8418ca391ae7ff377434924b7f3386d5a0623796
Paige McNamara-Pittler <pemcnama@usc.edu>
2025-10-22 13:43:32 -0700

    Remove duplicate @main entry point to fix build
    Removed obsolete file due to duplicate @main declaration.

--- JotItDownApp/JotItDownAppApp.swift
+++ JotItDownApp/JotItDownAppApp.swift
@@ -1,17 +1,2 @@
-//
-//  JotItDownAppApp.swift
-//  JotItDownApp
-//
-//  Created by Paige McNamara-Pittler on 9/16/25.
-//
-
-import SwiftUI
-
-@main
-struct JotItDownAppApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
+// JotItDownAppApp.swift
+// Obsolete file (removed duplicate @main)
