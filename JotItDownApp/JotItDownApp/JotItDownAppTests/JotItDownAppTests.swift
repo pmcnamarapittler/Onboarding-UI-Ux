f3a6bcbde5f28ab4e0bfb41d0c532e45aa753286
Paige McNamara-Pittler <pemcnama@usc.edu>
2025-11-10 13:54:19 -0800

    Add files via upload

---
+++ JotItDownApp/JotItDownApp/JotItDownAppTests/JotItDownAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  JotItDownAppTests.swift
+//  JotItDownAppTests
+//
+//  Created by Paige McNamara-Pittler on 9/16/25.
+//
+
+import Testing
+@testable import JotItDownApp
+
+struct JotItDownAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
